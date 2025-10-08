dd4d275af99480539100d2dee985f327c93e321d
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-08 20:03:55 +0200

    feat():AP-43 Create a generic DIC to register beams inside of main bootstrap

---
+++ src/main/java/config/DIContainer.java
@@ -0,0 +1,23 @@
+package config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DIContainer {
+    private static final DIContainer instance = new DIContainer();
+    private static final Map<Class<?>, Object> beans = new HashMap<>();
+
+    private DIContainer() {}
+
+    public DIContainer getInstance() {
+        return instance;
+    }
+
+    public <T> void registerBean(Class<T> clazz, T b) {
+        beans.put(clazz, b);
+    }
+
+    public <T> T getBean(Class<T> clazz) {
+        return (T) beans.get(clazz);
+    }
+}
