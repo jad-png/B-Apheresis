44c839a233859ce118c2bda22ec60e9dcfc18480
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-09 15:40:12 +0200

    feat():AP-54 create blood type enum

---
+++ src/main/java/entity/enums/BloodType.java
@@ -0,0 +1,12 @@
+package entity.enums;
+
+public enum BloodType {
+    O_POSITIVE,
+    O_NEGATIVE,
+    B_POSITIVE,
+    B_NEGATIVE,
+    A_POSITIVE,
+    A_NEGATIVE,
+    AB_POSITIVE,
+    AB_NEGATIVE
+}
