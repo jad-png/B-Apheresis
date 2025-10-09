4d60924e33eaf064930c35b0911d317bf094e13d
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-09 15:35:56 +0200

    feat():AP-50 create Account class with primary attributes and no enums

---
+++ src/main/java/entity/Account.java
@@ -0,0 +1,16 @@
+package entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Account {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String cin;
+    // bloodtype enum
+    // gender enum
+    private LocalDate birthday;
+    private LocalDateTime created_at;
+    
+}
