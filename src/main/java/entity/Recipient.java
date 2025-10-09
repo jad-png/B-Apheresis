96e2c4da82a79d0050b93678698337d6a044a9db
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-09 23:57:01 +0200

    feat():AP-60 define relationship attribute, and constructors

--- src/main/java/entity/Recipient.java
+++ src/main/java/entity/Recipient.java
@@ -1,7 +1,46 @@
 package entity;
 
-public class Recipient {
-    private int requiredBags;
-    private int currentBags;
+import entity.enums.*;
 
-}
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Table(name = "recipients")
+@PrimaryKeyJoinColumn(name = "account_id")
+public class Recipient extends Account {
+
+    @Column(name = "required_bags")
+    private Integer requiredBags;
+
+    @Column(name = "current_bags")
+    private Integer currentBags;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state")
+    private State state;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "medical_situation")
+    private Situation mcSituation;
+
+    @OneToMany(mappedBy = "recipient", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Donor> donors;
+
+    public Recipient() {
+        super();
+        this.currentBags = 0;
+        this.state = State.WAITING;
+    }
+
+    public Recipient(String firstName, String lastName, String cin, String phone,
+                     Date birthday, BloodType bloodType, Gender gender,
+                     Double weight, Situation mcSituation) {
+        super(firstName, lastName, cin, birthday, gender, bloodType);
+        this.mcSituation = mcSituation;
+        this.currentBags = 0;
+        this.state = state.WAITING;
+        this.requiredBags = 0;
+    }
+
+    }
