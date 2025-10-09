f55146042a849b605abf6d0661d3a1396beeafe2
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-09 18:07:43 +0200

    feat():AP-47 add getters/setters for donor's class

--- src/main/java/entity/Donor.java
+++ src/main/java/entity/Donor.java
@@ -1,5 +1,7 @@
 package entity;
 
+import entity.enums.BloodType;
+import entity.enums.Gender;
 import entity.enums.MedicalCondition;
 import entity.enums.Status;
 
@@ -27,12 +29,36 @@ public class Donor extends Account {
     @Column(name = "medical_condition")
     private MedicalCondition mdCondition;
 
+    // TODO: create recipient class and type this attribute with it
     // many donors can donate to one recipient relation
-    // @JoinColumn(name = "revipient_id")
+//    @ManyToOne
     // recipient fk
+//    @JoinColumn(name = "revipient_id")
 
     public Donor() {
         super();
-        // define default status
+        this.status = Status.AVAILABLE;
     }
+
+    public Donor(String firstName, String lastName, String cin, String phone,
+                 Date birthday, BloodType bloodType, Gender gender,
+                 Double weight, MedicalCondition medicalCondition) {
+        super(firstName, lastName, cin, birthday, gender, bloodType);
+        this.weight = weight;
+        this.mdCondition = medicalCondition;
+        this.status = Status.AVAILABLE;
+    }
+
+    public Status getStatus() { return status; }
+    public void setStatus(Status status) { this.status = status; }
+
+    public Date getLastDonationDate() { return lastDonationDate; }
+    public void setLastDonationDate(Date lastDonationDate) {
+        this.lastDonationDate = lastDonationDate;
+    }
+
+    public MedicalCondition getMedicalCondition() { return medicalCondition; }
+    // setter for medical COndition
+
+    // here getter/setter for recipient
 }
