36f6935ecc038c626e20047948f230b57e942bb4
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-10 12:29:12 +0200

    feat(utils):AP-68 create a static initializer block that creates and EMF using configuration from persistance xml

--- src/main/java/utils/JPAUtils.java
+++ src/main/java/utils/JPAUtils.java
@@ -1,8 +1,17 @@
 package utils;
 
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class JPAUtils {
     private static final EntityManagerFactory emf;
-    
+
+    static {
+        try {
+        emf = Persistence.createEntityManagerFactory("B-ApheresisPU");
+        } catch (Exception e) {
+            System.err.println("EntityManagerFactory initialization failed: " + e.getMessage());
+            throw new ExceptionInInitializerError(e);
+        }
+    }
 }
