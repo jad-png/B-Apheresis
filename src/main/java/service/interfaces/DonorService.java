194d0b662557e1f4c0d6447fa888c43c3d1aab90
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-10 16:22:42 +0200

    feat(service):AP-77 create donor service interface

---
+++ src/main/java/service/interfaces/DonorService.java
@@ -0,0 +1,31 @@
+package service.interfaces;
+
+import dto.DonorDTO;
+import entity.Donor;
+import entity.enums.BloodType;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DonorService {
+
+    public boolean isEligible(Donor donor);
+    public boolean canDonate(Donor donor);
+    public void updateDonorStatus(Donor donor);
+
+    // service methods
+    public DonorDTO createDonor(DonorDTO dto);
+    public List<DonorDTO> getAllDonors();
+    public Optional<DonorDTO> getDonorById(Long id);
+    public DonorDTO updateDonor(DonorDTO donorDTO);
+    public void deleteDonor(Long id);
+
+    public List<DonorDTO> getAvailableDonors();
+    public Optional<DonorDTO> getEligibleDonors();
+    public List<DonorDTO> getDonorsByBloodType(BloodType bloodType);
+
+    // helpers
+//    public boolean isCinUnique(String cin);
+//  public Optional<DonorDTO> getDonorByCin(String cin);
+//    public void close();
+}
