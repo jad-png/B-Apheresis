0d13c8c1e2e7d4c714df90a51562bd772a639963
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-10 11:50:39 +0200

    feat(Dao):AP-65 Define business specific queries in Dao interface

--- src/main/java/dao/interfaces/DonorDao.java
+++ src/main/java/dao/interfaces/DonorDao.java
@@ -1,6 +1,8 @@
 package dao.interfaces;
 
 import entity.Donor;
+import entity.enums.BloodType;
+import entity.enums.Status;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +14,13 @@ interface DonorDao {
     List<Donor> lsAll();
     Donor update(Donor donor);
     void delete(Long id);
+
+    // business apecific queries
+    List<Donor> findByStatus(Status status);
+    List<Donor> findByBloodType(BloodType bloodType);
+    List<Donor> findByMedicalCondition(String condition);
+    Optional<Donor> findByCin(String cin);
+    List<Donor> findEligibleDonors();
+    List<Donor> findAvailableDonors();
+    List<Donor> findByRecipient(Long recipientId);
 }
