06d4327331a47c1b49da9913aacac5253f88bb3b
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-11 17:58:21 +0200

    feat(dto):AP-88 add getters and setters for both. Inherited parent class attributes and class base attributes

--- src/main/java/dto/RecipientDTO.java
+++ src/main/java/dto/RecipientDTO.java
@@ -24,5 +24,40 @@ public class RecipientDTO {
 
     public RecipientDTO() {}
 
-    
+    // parent class Account attributes
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getFistName() { return fistName; }
+    public void setFistName(String fistName) { this.fistName = fistName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getCin() { return cin; }
+    public void setCin(String cin) { this.cin = cin; }
+
+    public BloodType getBloodType() { return bloodType; }
+    public void setBloodType(BloodType bloodType) { this.bloodType = bloodType; }
+
+    public Gender getGender() { return gender; }
+    public void setGender(Gender gender) { this.gender = gender; }
+
+    public Date getBirthday() { return birthday; }
+    public void setBirthday(Date birthday) { this.birthday = birthday; }
+
+    // Recipient attributes
+    public Integer getRequiredBags() { return requiredBags; }
+    public void setRequiredBags(Integer requiredBags) { this.requiredBags = requiredBags; }
+
+    public Integer getCurrentBags() { return currentBags; }
+    public void setCurrentBags(Integer currentBags) { this.currentBags = currentBags; }
+
+    public State getState() { return state; }
+    public void setState(State state) { this.state = state; }
+
+    public MedicalCondition getMdCondition() { return mdCondition; }
+    public void setMdCondition(MedicalCondition mdCondition) { this.mdCondition = mdCondition; }
+
+    public List<DonorDTO> getDonors() { return donors; }
 }
