0e89d10eebf43ebd1da0fd3a52fcf78c408d93bd
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-12 05:17:02 +0200

    feat(service):AP-128 extends from loggable and implements DonationService interface

--- src/main/java/service/impl/DonationServiceImpl.java
+++ src/main/java/service/impl/DonationServiceImpl.java
@@ -1,9 +1,15 @@
 package service.impl;
 
 import dao.impl.DonationDaoImpl;
+import dto.DonationDTO;
 import mapper.DonationMapper;
+import service.interfaces.DonationService;
+import utils.Loggable;
 
-public class DonationServiceImpl {
+import java.util.Collections;
+import java.util.List;
+
+public class DonationServiceImpl extends Loggable implements DonationService {
     private final DonationDaoImpl dao;
     private final DonationMapper mapper;
 
@@ -13,4 +19,33 @@ public class DonationServiceImpl {
     }
 
 
+    @Override
+    public DonationDTO saveDonation(DonationDTO dto) {
+        return null;
+    }
+
+    @Override
+    public void deleteDonation(Long id) {
+
+    }
+
+    @Override
+    public List<DonationDTO> getDonation(Long id) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<DonationDTO> getAllDonations() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<DonationDTO> getByDonatorId(Long donatorId) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<DonationDTO> getByRecipientId(Long id) {
+        return Collections.emptyList();
+    }
 }
