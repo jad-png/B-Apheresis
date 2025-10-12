f177757e3c706f6cf60513bcab56281728853646
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-12 04:12:11 +0200

    feat(mapper):AP-121 toDto method Implmeneted --start--

--- src/main/java/mapper/DonationMapper.java
+++ src/main/java/mapper/DonationMapper.java
@@ -4,5 +4,16 @@ import dto.DonationDTO;
 import entity.Donation;
 
 public class DonationMapper {
+    public DonationDTO toDto (Donation donation) {
+        if (donation == null) return null;
+
+        DonationDTO dto = new DonationDTO();
+        dto.setId(donation.getId());
+        dto.setDonorId(donation.getDonor().getId());
+        dto.setRecipientId(donation.getRecipient().getId());
+
+        return dto;
+    }
+
     
 }
