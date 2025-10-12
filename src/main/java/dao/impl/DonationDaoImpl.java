12cfe07a80b3cacd65d50cc2e839dd85e24dd97e
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-12 04:38:06 +0200

    feat(dao):AP-124 Extends Loggable for logs, and implements DonationDao interface

--- src/main/java/dao/impl/DonationDaoImpl.java
+++ src/main/java/dao/impl/DonationDaoImpl.java
@@ -1,11 +1,17 @@
 package dao.impl;
 
 import config.DIContainer;
+import dao.interfaces.DonationDao;
+import entity.Donation;
+import utils.Loggable;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
-public class DonationDaoImpl {
+public class DonationDaoImpl extends Loggable implements DonationDao {
     private final EntityManager em;
 
     public DonationDaoImpl() {
@@ -13,5 +19,34 @@ public class DonationDaoImpl {
         em = emf.createEntityManager();
     }
 
-    
+
+    @Override
+    public Donation save(Donation donation) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public Optional<Donation> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Donation> findAll() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<Donation> findByDonationId(Long id) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<Donation> findByRecipientId(Long id) {
+        return Collections.emptyList();
+    }
 }
