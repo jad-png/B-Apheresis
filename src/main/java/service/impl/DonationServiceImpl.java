1e9cf72420212e1ebf40f9ddc1a814ee9ba96cac
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-12 05:23:00 +0200

    feat(service):AP-128 implemented business logic for implemented methods

--- src/main/java/service/impl/DonationServiceImpl.java
+++ src/main/java/service/impl/DonationServiceImpl.java
@@ -2,12 +2,15 @@ package service.impl;
 
 import dao.impl.DonationDaoImpl;
 import dto.DonationDTO;
+import entity.Donation;
 import mapper.DonationMapper;
 import service.interfaces.DonationService;
 import utils.Loggable;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class DonationServiceImpl extends Loggable implements DonationService {
     private final DonationDaoImpl dao;
@@ -21,31 +24,53 @@ public class DonationServiceImpl extends Loggable implements DonationService {
 
     @Override
     public DonationDTO saveDonation(DonationDTO dto) {
-        return null;
+        logMethodEntry("saveDonation", dto);
+        Donation donation = mapper.toEntity(dto);
+        Donation saved = dao.save(donation);
+        DonationDTO result = mapper.toDto(saved);
+        logMethodExit("saveDonation", result);
+        return result;
     }
 
     @Override
     public void deleteDonation(Long id) {
-
+        logMethodEntry("deleteDonation", id);
+        dao.delete(id);
+        logMethodExit("deleteDonation", id);
     }
 
     @Override
-    public List<DonationDTO> getDonation(Long id) {
-        return Collections.emptyList();
+    public Optional<DonationDTO> getDonation(Long id) {
+        logMethodEntry("getDonation", id);
+        Optional<DonationDTO> dto = dao.findById(id).map(mapper::toDto);
+        logMethodExit("getDonation", dto);
+        return dto;
     }
 
     @Override
     public List<DonationDTO> getAllDonations() {
-        return Collections.emptyList();
+        logMethodEntry("getAllDonations");
+        List<DonationDTO> list = dao.findAll().stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
+        return list;
     }
 
     @Override
     public List<DonationDTO> getByDonatorId(Long donatorId) {
-        return Collections.emptyList();
+        logMethodEntry("getByDonatorId", donatorId);
+        List<DonationDTO> list = dao.findByDonationId(donatorId).stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
+        return list;
     }
 
     @Override
     public List<DonationDTO> getByRecipientId(Long id) {
-        return Collections.emptyList();
+        logMethodEntry("getByRecipientId", id);
+        List<DonationDTO> list = dao.findByRecipientId(id).stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
+        return list;
     }
 }
--- src/main/java/service/interfaces/DonationService.java
+++ src/main/java/service/interfaces/DonationService.java
@@ -3,11 +3,12 @@ package service.interfaces;
 import dto.DonationDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DonationService {
     DonationDTO saveDonation(DonationDTO dto);
     void deleteDonation(Long id);
-    List<DonationDTO> getDonation(Long id);
+    Optional<DonationDTO> getDonation(Long id);
     List<DonationDTO> getAllDonations();
     List<DonationDTO> getByDonatorId(Long donatorId);
     List<DonationDTO> getByRecipientId(Long id);
