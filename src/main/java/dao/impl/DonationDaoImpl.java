a40720675b34e73d83acfcb37904c58c8f42962e
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-12 04:51:07 +0200

    feat(dao):AP-124 Implemented DonationDao interface

--- src/main/java/dao/impl/DonationDaoImpl.java
+++ src/main/java/dao/impl/DonationDaoImpl.java
@@ -7,6 +7,7 @@ import utils.Loggable;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -22,31 +23,97 @@ public class DonationDaoImpl extends Loggable implements DonationDao {
 
     @Override
     public Donation save(Donation donation) {
-        return null;
+        logMethodEntry("save", donation);
+        try {
+            em.getTransaction().begin();
+            Donation saved = em.merge(donation);
+            em.getTransaction().commit();
+            logMethodExit("save", saved);
+            return saved;
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            logMethodExit("save", e);
+            throw e;
+        }
     }
 
     @Override
     public void delete(Long id) {
-
+        logMethodEntry("delete", id);
+        try {
+            Donation donation = em.find(Donation.class, id);
+            if (donation != null) {
+                em.getTransaction().begin();
+                em.refresh(donation);
+                em.remove(donation);
+            }
+        } catch (Exception e) {
+            logMethodExit("Error while deleting", e);
+        }
     }
 
     @Override
     public Optional<Donation> findById(Long id) {
-        return Optional.empty();
+        logMethodEntry("findById", id);
+        try {
+            Donation donation = em.find(Donation.class, id);
+            logMethodExit("findById", donation);
+            return Optional.of(donation);
+        } catch (Exception e) {
+            logError("Error finding donation by Id", e);
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Donation> findAll() {
-        return Collections.emptyList();
+        logMethodEntry("findAll");
+        try {
+            TypedQuery<Donation> query = em.createQuery(
+                    "SELECT d FROM Donation d", Donation.class
+            );
+
+            List<Donation> donations = query.getResultList();
+            logMethodExit("findAll", donations);
+            return donations;
+        } catch (Exception e) {
+            logError("Error finding all donations", e);
+            return Collections.emptyList();
+        }
     }
 
     @Override
     public List<Donation> findByDonationId(Long id) {
-        return Collections.emptyList();
+        logMethodEntry("findByDonationId", id);
+        try {
+            TypedQuery<Donation> query = em.createQuery(
+                    "SELECT d FROM Donation d WHERE d.donor.id = :id", Donation.class
+            );
+            query.setParameter("id", id).getResultList();
+            List<Donation> donations = query.getResultList();
+            logMethodExit("findByDonationId", donations);
+            return donations;
+        } catch (Exception e) {
+            logError("Error finding all donations", e);
+            return Collections.emptyList();
+        }
     }
 
     @Override
     public List<Donation> findByRecipientId(Long id) {
-        return Collections.emptyList();
+        logMethodEntry("findByRecipientId", id);
+        try {
+            TypedQuery<Donation> query = em.createQuery(
+                    "SELECT d FROM Donation d WHERE d.recipient.id = :id", Donation.class
+            );
+
+            query.setParameter("id", id);
+            List<Donation> donations = query.getResultList();
+            logMethodExit("findByRecipientId", donations);
+            return donations;
+        } catch (Exception e) {
+            logError("Error finding all donations", e);
+            return Collections.emptyList();
+        }
     }
 }
