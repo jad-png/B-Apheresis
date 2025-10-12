f6e90dcf505c035e127f953e1a95024852952087
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-12 03:32:07 +0200

    feat(entity):AP-115 Create Donation entity class and define attributes and relation between both Donor and Recipient

---
+++ src/main/java/entity/Donation.java
@@ -0,0 +1,20 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "donations")
+public class Donation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "donator_id")
+    private Account donor;
+
+    @ManyToOne
+    @JoinColumn(name = "reviewer_id")
+    private Account recipient;
+    
+}
