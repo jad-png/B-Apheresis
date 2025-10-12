cb8312e19f8c373a1955abd70c75b5042121b4af
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-12 03:47:09 +0200

    feat(dto):AP-110 AP-120 Encapsulated dto atrributes --finish--

--- src/main/java/dto/DonationDTO.java
+++ src/main/java/dto/DonationDTO.java
@@ -7,4 +7,13 @@ public class DonationDTO {
     private Long recipientId;
 
     public DonationDTO() {}
+
+    public Long getId() { return id;}
+    public void setId(Long id) {  this.id = id; }
+
+    public Long getDonorId() { return donorId; }
+    public void setDonorId(Long donorId) { this.donorId = donorId; }
+
+    public Long getRecipientId() { return recipientId; }
+    public Long setRecipientId(Long recipientId) { this.recipientId = recipientId; return this.id; }
 }
