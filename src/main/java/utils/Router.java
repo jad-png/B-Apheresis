9135785fdb675f059b8c2bca794529505fee7121
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-14 12:25:16 +0200

    feat(router):AP-145 Implement goTo method that request a dispather for full path and forward it's req and res

--- src/main/java/utils/Router.java
+++ src/main/java/utils/Router.java
@@ -1,5 +1,15 @@
 package utils;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class Router {
-    
+    public static void goTo(HttpServletResponse res, HttpServletRequest req, String path) throws ServletException, IOException {
+        String fullPath = "/WEB-INF/" + path + ".jsp";
+        RequestDispatcher dispatcher = req.getRequestDispatcher(fullPath);
+        dispatcher.forward(req, res);
+    }
 }
