9d5a362e523084ae1267101ff55f1bc1d26b7031
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-15 16:09:40 +0200

    feat(servlet, recipient):AP-180 implement show edit and create form methods with handleFilter method left empty

--- src/main/java/servlets/RecipientServlet.java
+++ src/main/java/servlets/RecipientServlet.java
@@ -2,9 +2,17 @@ package servlets;
 
 import config.DIContainer;
 import controller.RecipientController;
+import dto.RecipientDTO;
+import entity.Recipient;
 import mapper.RecipientMapper;
+import utils.Router;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
 
 public class RecipientServlet extends HttpServlet {
     private RecipientController controller;
@@ -14,6 +22,36 @@ public class RecipientServlet extends HttpServlet {
     public void init() {
         this.controller = DIContainer.getInstance().getBean(RecipientController.class);
         this.mapper = DIContainer.getInstance().getBean(RecipientMapper.class);
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    }
+
+    //    --------- Views -------
+    private void showCreateForm(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        Router.goTo(res, req, "/recipient/create");
+    }
+
+    private void showEditForm(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        Long id = Long.parseLong(req.getParameter("id"));
+
+        Optional<RecipientDTO> recipientOpt = controller.getRecipientById(id);
+        recipientOpt.ifPresent(r -> req.setAttribute("recipient", r));
+        Router.goTo(res, req, "/recipient/edit");
+    }
+
+    private void handleFilter(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String filterType = req.getParameter("filterType");
+
+        if (filterType == null || filterType.isEmpty()) {
+            // if null list all recipient
+            return;
+        }
+
+        // switch case
     }
 
     
