0f39df1275a6f10ad06280db970b4e1bdaca0024
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-15 11:31:24 +0200

    feat(servlet):AP-177 implement method that getParametres values and set it in donorDTO

--- src/main/java/servlets/DonorServlet.java
+++ src/main/java/servlets/DonorServlet.java
@@ -3,6 +3,9 @@ package servlets;
 import config.DIContainer;
 import controller.DonorController;
 import dto.DonorDTO;
+import entity.enums.BloodType;
+import entity.enums.Gender;
+import entity.enums.MedicalCondition;
 import utils.Router;
 
 import javax.servlet.ServletException;
@@ -10,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -43,8 +47,37 @@ public class DonorServlet extends HttpServlet {
         Router.goTo(res, req, "/donor/list");
     }
 
+    private void handleFilter(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String bloodType = req.getParameter("bloodType");
+        List<DonorDTO> filtered = controller.getDonorsByBloodType(bloodType);
+        req.setAttribute("filtered", filtered);
+        Router.goTo(res, req, "/donor/list");
+    }
+
     // --------- Handlets ---------
     private void handleCreate(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        DonorDTO dto = new
+
+    }
+
+
+    // --------- Helpers ---------
+    private DonorDTO extractDonorFromRequest(HttpServletRequest req) throws ServletException, IOException {
+        DonorDTO dto = new DonorDTO();
+
+        if (req.getParameter("id") != null) {
+            dto.setId(Long.parseLong(req.getParameter("id")));
+        }
+
+        dto.setFirstName(req.getParameter("firstName"));
+        dto.setLastName(req.getParameter("lastName"));
+        dto.setCin(req.getParameter("cin"));
+
+        dto.setBirthday(LocalDate.parse(req.getParameter("birthday")));
+        dto.setBloodType(BloodType.valueOf(req.getParameter("bloodType")));
+        dto.setGender(Gender.valueOf(req.getParameter("gender")));
+        dto.setWeight(Double.parseDouble(req.getParameter("weight")));
+        dto.setMedicalCondition(MedicalCondition.valueOf(req.getParameter("medicalCondition")));
+
+        return dto;
     }
 }
