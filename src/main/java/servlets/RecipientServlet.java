2d276248d3eb394d9b8037e1670842fa466719f7
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-15 16:13:36 +0200

    feat(servlet, recipient):AP-180 implement list methods

--- src/main/java/servlets/RecipientServlet.java
+++ src/main/java/servlets/RecipientServlet.java
@@ -2,6 +2,7 @@ package servlets;
 
 import config.DIContainer;
 import controller.RecipientController;
+import dto.DonorDTO;
 import dto.RecipientDTO;
 import entity.Recipient;
 import mapper.RecipientMapper;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 public class RecipientServlet extends HttpServlet {
@@ -54,5 +56,37 @@ public class RecipientServlet extends HttpServlet {
         // switch case
     }
 
-    
+    // --------- list methods ---------
+    private void listAllRecipients(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        List<RecipientDTO> r = controller.getAllRecipients();
+        req.setAttribute("recipients", r);
+        Router.goTo(res, req, "/recipient/list");
+    }
+
+    private void listRecipientByBloodType(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String bloodType = req.getParameter("bloodType");
+        List<RecipientDTO> r = controller.getRecipientsByBloodType(bloodType);
+        req.setAttribute("recipients", r);
+        Router.goTo(res, req, "/recipient/list");
+    }
+
+    private void listRecipientByState(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String state = req.getParameter("state");
+        List<RecipientDTO> r = controller.getRecipientsByState(state);
+        req.setAttribute("recipients", r);
+        Router.goTo(res, req, "/recipient/list");
+    }
+
+    private void listRecipientBySituation(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String situation = req.getParameter("situation");
+        List<RecipientDTO> r = controller.getRecipientsBySituation(situation);
+        req.setAttribute("recipients", r);
+        Router.goTo(res, req, "/recipient/list");
+    }
+
+    private void listPriorityRecipients(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        List<RecipientDTO> r = controller.getRecipientsByPriority();
+        req.setAttribute("recipients", r);
+        Router.goTo(res, req, "/recipient/list");
+    }
 }
