25ad0a6b417ea2257ed76202ed0c6626567fa983
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-15 16:50:08 +0200

    feat(servlet, recipient):AP-185 implement method that map data from request into DTO

--- src/main/java/servlets/RecipientServlet.java
+++ src/main/java/servlets/RecipientServlet.java
@@ -5,6 +5,10 @@ import controller.RecipientController;
 import dto.DonorDTO;
 import dto.RecipientDTO;
 import entity.Recipient;
+import entity.enums.BloodType;
+import entity.enums.Gender;
+import entity.enums.Situation;
+import entity.enums.State;
 import mapper.RecipientMapper;
 import utils.Router;
 
@@ -13,6 +17,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -136,5 +141,28 @@ public class RecipientServlet extends HttpServlet {
         List<RecipientDTO> r = controller.getWaitingRecipients();
         req.setAttribute("recipients", r);
         Router.goTo(res, req, "/recipient/list");
+    }
+
+
+    // --------- helpers ---------
+    private RecipientDTO extractRecipientFromRequest(HttpServletRequest req) {
+        RecipientDTO dto = new RecipientDTO();
+
+        if (req.getParameter("id") != null && !req.getParameter("id").isEmpty()) {
+            dto.setId(Long.parseLong(req.getParameter("id")));
+        }
+
+        dto.setFirstName(req.getParameter("firstName"));
+        dto.setLastName(req.getParameter("lastName"));
+        dto.setCin(req.getParameter("cin"));
+        dto.setBloodType(BloodType.valueOf(req.getParameter("bloodType")));
+        dto.setGender(Gender.valueOf(req.getParameter("gender")));
+        dto.setBirthday(LocalDate.parse(req.getParameter("birthday")));
+        dto.setRequiredBags(Integer.parseInt(req.getParameter("requiredBags")));
+        dto.setCurrentBags(Integer.parseInt(req.getParameter("currentBags")));
+        dto.setState(State.valueOf(req.getParameter("state")));
+        dto.setSituation(Situation.valueOf(req.getParameter("situation")));
+
+        return dto;
     }
 }
